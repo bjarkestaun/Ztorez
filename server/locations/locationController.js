1ceb044ef4f00e92094aeacb2c189cf253aa518b
Bjarke Staun-Olsen <bjarkestaun@gmail.com>
2015-11-09 20:39:02 -0800

    getAllLocations api endpoint works

--- server/locations/locationController.js
+++ server/locations/locationController.js
@@ -1,10 +1,17 @@
 var Location = require('./locationModel.js');
 var geocodingHelper = require('../services/geocodingHelper.js');
+var Promise = require('bluebird');
 
 module.exports = {
   
   getAllLocations: function (req, res, next) {
-    res.send(200);
+    Promise.resolve(Location.find().exec())
+      .then(function (locations) {
+        res.send(200, locations);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   },
 
   getBrandedLocations: function (req, res, next) {
